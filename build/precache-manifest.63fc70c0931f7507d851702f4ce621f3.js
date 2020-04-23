02cf73f74c4cea72ee9009fac8a8810d2b8cc74c
Nikita Holovnin <Golovninnikita@ukr.net>
2020-04-23 17:15:39 +0300

    made build

---
+++ build/precache-manifest.63fc70c0931f7507d851702f4ce621f3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed7f247c739ec68f0533",
+    "url": "/static/js/0.6092e301.chunk.js"
+  },
+  {
+    "revision": "da30b4766c3aaf1504c1",
+    "url": "/static/css/main.3681b8fe.chunk.css"
+  },
+  {
+    "revision": "da30b4766c3aaf1504c1",
+    "url": "/static/js/main.c8b5d9f7.chunk.js"
+  },
+  {
+    "revision": "f194528aafe27b40f3f6",
+    "url": "/static/js/make_string-page.dcd08b98.chunk.js"
+  },
+  {
+    "revision": "c08532a3d21dd463725b",
+    "url": "/static/js/runtime~main.e8388f2a.js"
+  },
+  {
+    "revision": "883757999a27361fdde4",
+    "url": "/static/js/strings-home.113a47cf.chunk.js"
+  },
+  {
+    "revision": "4d51ae429e18538f69e4",
+    "url": "/static/css/5.48925cc7.chunk.css"
+  },
+  {
+    "revision": "4d51ae429e18538f69e4",
+    "url": "/static/js/5.80458e93.chunk.js"
+  },
+  {
+    "revision": "e89a6277a12445e01402e74dde6029ac",
+    "url": "/static/media/UGPay logo.e89a6277.png"
+  },
+  {
+    "revision": "0742c0fac5c2c8402f10b61d25ec3dec",
+    "url": "/index.html"
+  }
+];
